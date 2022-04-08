f81fb7e7425d4f830aaea41c7cd352c87b3c6d72
konojunya <junya03dance@gmail.com>
2022-04-08 15:28:42 +0900

    feat: dice

--- api/index.rs
+++ api/index.rs
@@ -1,18 +1,22 @@
 use http::StatusCode;
+use rand::{thread_rng, Rng};
 use std::error::Error;
 use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
 
 fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
+    let mut rng = thread_rng();
+
     let response = Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "text/plain")
-        .body("Hello Rust API")
+        .status(StatusCode::FOUND)
+        .header("Location", format!("/{}.png", rng.gen_range(1..7)))
+        // https://vercel.com/docs/concepts/edge-network/caching
+        .header("Cache-Control", "no-cache")
+        .body("")
         .expect("Internal Server Error");
 
     Ok(response)
 }
 
-// Start the runtime with the handler
 fn main() -> Result<(), Box<dyn Error>> {
     Ok(lambda!(handler))
 }
