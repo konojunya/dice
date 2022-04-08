5b2b2567f7c5588a8db07d44f82f209557d579d4
konojunya <junya03dance@gmail.com>
2022-04-08 14:44:42 +0900

    feat: change body

--- api/user.rs
+++ api/user.rs
@@ -1,18 +1,18 @@
-use http::{StatusCode};
-use vercel_lambda::{lambda, error::VercelError, IntoResponse, Request, Response};
+use http::StatusCode;
 use std::error::Error;
+use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
 
 fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
-	let response = Response::builder()
-		.status(StatusCode::OK)
-		.header("Content-Type", "text/plain")
-		.body("Hello World")
-		.expect("Internal Server Error");
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "text/plain")
+        .body("Hello Rust API")
+        .expect("Internal Server Error");
 
-		Ok(response)
+    Ok(response)
 }
 
 // Start the runtime with the handler
 fn main() -> Result<(), Box<dyn Error>> {
-	Ok(lambda!(handler))
+    Ok(lambda!(handler))
 }
